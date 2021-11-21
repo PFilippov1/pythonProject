5f1e9ee3a8030e839c69516bad70465488b41eb1
Pavel Filippov <p.filippov2.20@gmail.com>
2021-11-21 22:23:11 +0300

    second

--- lab1.py
+++ lab1.py
@@ -7,14 +7,14 @@ frase1 = "мы нашли " + str(k) + " грибов"
 frase2 = "мы нашли " + str(k) + " гриба"
 frase3 = "мы нашли " + str(k) + " гриб"
 
-if x[0] >= 1 and x[-1] == 1:
+if x[0] >= 2 and x[-1] == 1:
     print(frase3)
-elif x[0] == 0 and x[-1] == 0:
+elif (x[0] == 0 and x[-1] == 0) or (x[0] > 0 and x[-1] == 0) or (x[0] > 0 and x[-1] > 0 < 5):
     print(frase1)
-elif x[0] <= 4 and x[-1] <= 4:
+elif (x[0] <= 4 and x[-1] <= 4) :
     print(frase2)
-elif x[0] > 0 and x[-1] == 0:
-    print(frase1)
+# elif x[0] > 0 and x[-1] == 0:
+#     print(frase1)
 else:
     print(frase1)
-    #print("Я уже весь в грыбах, грибов, грибей...")
+    # print("Я уже весь в грыбах, грибов, грибей...")
