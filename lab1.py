16b268d5e4668b7c75efcc660dcdd26388fd89f2
Pavel Filippov <p.filippov2.20@gmail.com>
2021-11-21 22:01:17 +0300

    first

--- lab1.py
+++ lab1.py
@@ -1,1 +1,20 @@
-print("hello world")
+k = int(input("Введите число\n"))
+num = k
+x = [int(a) for a in str(num)]
+print(x)
+
+frase1 = "мы нашли " + str(k) + " грибов"
+frase2 = "мы нашли " + str(k) + " гриба"
+frase3 = "мы нашли " + str(k) + " гриб"
+
+if x[0] >= 1 and x[-1] == 1:
+    print(frase3)
+elif x[0] == 0 and x[-1] == 0:
+    print(frase1)
+elif x[0] <= 4 and x[-1] <= 4:
+    print(frase2)
+elif x[0] > 0 and x[-1] == 0:
+    print(frase1)
+else:
+    print(frase1)
+    #print("Я уже весь в грыбах, грибов, грибей...")
