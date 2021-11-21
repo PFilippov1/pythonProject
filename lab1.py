79b1a7dda5ec3f7a7eb7fb7454f018a689ef9a6c
Pavel Filippov <p.filippov2.20@gmail.com>
2021-11-21 22:35:47 +0300

    third

--- lab1.py
+++ lab1.py
@@ -7,12 +7,13 @@ frase1 = "мы нашли " + str(k) + " грибов"
 frase2 = "мы нашли " + str(k) + " гриба"
 frase3 = "мы нашли " + str(k) + " гриб"
 
-if x[0] >= 2 and x[-1] == 1:
+if (x[0] >= 2 and x[-1] == 1) or (k == 1):
     print(frase3)
+elif x[0] <= 4 and x[-1] <= 4:
+    print(frase2)
 elif (x[0] == 0 and x[-1] == 0) or (x[0] > 0 and x[-1] == 0) or (x[0] > 0 and x[-1] > 0 < 5):
     print(frase1)
-elif (x[0] <= 4 and x[-1] <= 4) :
-    print(frase2)
+
 # elif x[0] > 0 and x[-1] == 0:
 #     print(frase1)
 else:
