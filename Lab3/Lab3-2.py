117756d528c542c0f8699fbde32813eeb7be45e2
Pavel Filippov <p.filippov2.20@gmail.com>
2022-01-12 16:49:02 +0300

    lab3-2 almost

--- Lab3/Lab3-2.py
+++ Lab3/Lab3-2.py
@@ -1,5 +1,5 @@
-# Частично подсмотрено, не хочу копипастить 1:1 с рекурсией печаль.(, не заходит в цикл..(
-
+# Частично подсмотрено, не хочу копипастить 1:1 с рекурсией печаль.(, не заходит в цикл..( 2 последних варианта
+# собраны из кусков с WEB.
 
 length = int(input("Длина:  "))
 width = int(input("Ширина: "))
@@ -22,3 +22,47 @@ def slice_to_qudrats(s):
 
 print(f" стороны квадратов {result} ")
 print(f" Количество квадратов {len(result)} ")
+
+
+
+# a = int(input("Длина:  "))
+# b = int(input("Ширина: "))
+#
+# area = a * b
+#
+# def slice_to_qudrats(area):
+#     if area == 0:
+#         raise StopIteration
+#     while(area):
+#         for i in range(area, 0, -1):
+#             if i * i <= area:
+#                 area -= i * i
+#                 yield i
+#                 break
+#         slice_to_qudrats(area)
+#
+# result = list(slice_to_qudrats(area))
+# print(f" стороны квадратов {result} ")
+# print(f" Количество квадратов {len(result)} ")
+
+
+#
+# length = int(input("Длина:  "))
+# width = int(input("Ширина: "))
+#
+# result = []
+#
+# while (length > 0):
+#     if length < width:
+#         length, width = width, length
+#     result.append(width)
+#     length -= width
+#
+
+# a=len(result)
+# print(f" стороны квадратов {result} ")
+# print(f" Количество квадратов {len(result)} ")
+
+
+
+
