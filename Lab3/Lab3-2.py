6a3a3fa59a1a4a2bd475b14f4c5e4e3307e5dab1
Pavel Filippov <p.filippov2.20@gmail.com>
2022-01-22 14:21:05 +0300

    fix_Lab3 +Lab4

--- Lab3/Lab3-2.py
+++ Lab3/Lab3-2.py
@@ -1,68 +1,44 @@
 # Частично подсмотрено, не хочу копипастить 1:1 с рекурсией печаль.(, не заходит в цикл..( 2 последних варианта
 # собраны из кусков с WEB.
 
-length = int(input("Длина:  "))
-width = int(input("Ширина: "))
 
-result = []
-s = length * width
+a = int(input("Длина:  "))
+b = int(input("Ширина: "))
 
-break_out_flag = False
-def slice_to_qudrats(s):
-    if s == 0:
-      raise StopIteration
-    else:
-        while s > 0:
-            for i in range(s, 0, -1):
-                if length < width:
-                    length, width = width, length
-    result.append(width)
-    length -= width
-    slice_to_qudrats(s)
+area = a * b
 
+def slice_to_qudrats(area):
+    if area == 0:
+        raise StopIteration
+    while(area):
+        for i in range(area, 0, -1):
+            if i * i <= area:
+                area -= i * i
+                yield i
+                break
+        slice_to_qudrats(area)
+
+result = list(slice_to_qudrats(area))
 print(f" стороны квадратов {result} ")
 print(f" Количество квадратов {len(result)} ")
 
 
 
-# a = int(input("Длина:  "))
-# b = int(input("Ширина: "))
-#
-# area = a * b
-#
-# def slice_to_qudrats(area):
-#     if area == 0:
-#         raise StopIteration
-#     while(area):
-#         for i in range(area, 0, -1):
-#             if i * i <= area:
-#                 area -= i * i
-#                 yield i
-#                 break
-#         slice_to_qudrats(area)
-#
-# result = list(slice_to_qudrats(area))
-# print(f" стороны квадратов {result} ")
-# print(f" Количество квадратов {len(result)} ")
+
+#+ подсмотрено у Яны
+
+def rectangle(a, b, n=0):
+    if a == b:
+        return n + 1
+    elif a < b:
+        return rectangle(a, b - a, n + 1)
+    return rectangle(a - b, b, n + 1)
 
 
-#
-# length = int(input("Длина:  "))
-# width = int(input("Ширина: "))
-#
-# result = []
-#
-# while (length > 0):
-#     if length < width:
-#         length, width = width, length
-#     result.append(width)
-#     length -= width
-#
+a = int(input("Сторона а=\n"))
+b = int(input("Сторона b=\n"))
 
-# a=len(result)
-# print(f" стороны квадратов {result} ")
-# print(f" Количество квадратов {len(result)} ")
-
+print(f"Прямоугольнику с длинами {a} и {b} можно нарезать {rectangle(a, b)} квадратов")
 
 
 
