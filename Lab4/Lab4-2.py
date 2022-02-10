bba1d3e33b2161264253d1e9240aaa024ed43ac9
Pavel Filippov <p.filippov2.20@gmail.com>
2022-02-10 09:41:32 +0300

    lab4 last

--- Lab4/Lab4-2.py
+++ Lab4/Lab4-2.py
@@ -4,27 +4,28 @@
 # Kласс Car: id, Марка, Модель, Год выпуска, Цвет, Цена, Регистрационный номер
 
 # Создать список объектов. Вывести:
-# a)	список автомобилей заданной марки;
+# a) список автомобилей заданной марки;
 # б) список автомобилей заданной модели, которые эксплуатируются больше n лет;
 
 
 class Car:
-    id = 0
+    # id = 0
     cars = []
 
     # конструктор
-    def __init__(self, brand, model, year, color, price):
+    def __init__(self, id, brand, model, year, color, price):
+        self.id = id
         self.brand = brand
         self.model = model
         self.year = year
         self.color = color
         self.price = price
         self.__regNumber = 0  # вроде self.__regNumber = 0 можно и не указывать, т.к. будет сеттер
-        Car.id += 1
+        # Car.id += 1
         self.cars.append(self)
 
     def getBrand(self):
-        return self.brand
+        return brand
 
     def getModel(self):
         return model
@@ -46,23 +47,23 @@ class Car:
     def setRegNumber(self, regNumber):
         self.__regNumber = regNumber
 
-    # метод класса который будет каждый раз выводить ID+1 при создании объекта
-    @classmethod
-    def print_cars_id(cls):
-        print(f"Car id is: {cls.id}")
+    # метод класса который будет каждый раз выводить ID+1 при создании объекта (решил ниже жестко прописать для этого примера id )
+    # @classmethod
+    # def print_cars_id(cls):
+    #     print(f"Car id is: {cls.id}")
 
     # Создадим статический метод для расчета скидки клиента
     @staticmethod
     def customer_discount(price):
         return price - (price * discount)
 
-    # Создадим метод класса для вывода заданной марки авто
+    # Создадим метод класса для вывода заданной марки авто (не понял почему не могу в вывод добавить color price..  )
     @classmethod
     def print_list_brand(cls, brand: str):
         print(f"Список автомобилей заданной марки:\n")
         for car in cls.cars:
             if car.brand == brand:
-                print(f"{car.id} {car.brand} {car.model} {car.year} ")
+                print(f"{car.id} {car.brand} {car.model} {car.year}")
 
     # Создадим метод класса для вывода заданной марки авто
     @classmethod
@@ -73,61 +74,53 @@ class Car:
                 print(f"{car.id} {car.brand} {car.model} {car.year}")
 
 
-Car("toyota", "auris", 2017, "red", 12000)
-Car("mazda", "cx-5", 2020, "black", 22000)
-Car("toyota", "Corolla ", 2018, "red", 21000)
-Car("mazda", "cx-5", 2018, "white", 21000)
-Car("toyota", "Camry ", 2016, "green", 14000)
-Car("mazda", "cx-5", 2015, "blue", 16000)
-Car("toyota", "Highlander", 2020, "blue", 28000)
-Car("mazda", "cx-5", 2020, "grey", 22000)
-Car("toyota", "RAV4", 2019, "black", 22000)
-Car("mazda", "cx-5", 2017, "blue", 18000)
-Car("toyota", "Land Cruiser Prado", 2019, "black", 30000)
-Car("mazda", "cx-5", 2019, "white", 20000)
-
-
-
-
-
+Car(1, "toyota", "auris", 2017, "red", 12000)
+Car(2, "mazda", "cx-5", 2020, "black", 22000)
+Car(3, "toyota", "Corolla ", 2018, "red", 21000)
+Car(4, "mazda", "cx-5", 2018, "white", 21000)
+Car(5, "toyota", "Camry ", 2016, "green", 14000)
+Car(6, "mazda", "cx-5", 2015, "blue", 16000)
+Car(7, "toyota", "Highlander", 2020, "blue", 28000)
+Car(8, "mazda", "cx-5", 2020, "grey", 22000)
+Car(9, "toyota", "RAV4", 2019, "black", 22000)
+Car(10, "mazda", "cx-5", 2017, "blue", 18000)
+Car(11, "toyota", "Land Cruiser Prado", 2019, "black", 30000)
+Car(12, "mazda", "cx-5", 2019, "white", 20000)
 
 # Вывод по марке
 Car.print_list_brand("toyota")
 
-# Вывод по модели и возрасту
-Car.print_list_model_years_old("cx-5", 2)
+# Вывод по модели и сроку эксплуатации
+Car.print_list_model_years_old("cx-5", 4)
 
+car1 = Car(1, "toyota", "auris", 2017, "red", 12000)
+car2 = Car(2, "mazda", "cx-5", 2020, "blue", 22000)
 
+print("please set years experience of our service")
 
+discount = int(input())
+
+if (discount >= 0) and (discount < 2):
+    discount = 0.05
+elif (discount >= 2) and (discount < 5):
+    discount = 0.1
+elif discount >= 5:
+    discount = 0.2
+else:
+    print("please enter correct value")
 
 # car1 = Car("toyota", "auris", 2017, "red", 12000)
-# car2 = Car("mazda", "cx-5", 2020, "blue", 22000)
-#
-# print("please set years experience of our service")
-#
-# discount = int(input())
-#
-# if (discount >= 0) and (discount < 2):
-#     discount = 0.05
-# elif (discount >= 2) and (discount < 5):
-#     discount = 0.1
-# elif discount >= 5:
-#     discount = 0.2
-# else:
-#     print("please enter correct value")
-#
-# car1 = Car("toyota", "auris", 2017, "red", 12000)
 # Car.print_cars_id()
-# # добавляем рег. номер через сеттер
-# car1.setRegNumber("22-ff-44")
-# print(f" {car1.brand}, {car1.model}, {car1.year}, {car1.year}, {car1.price}, {car1.getRegNumber()}")
-# # посчитаем скидку
-# print(Car.customer_discount(car1.price))
-#
-# car2 = Car("mazda", "cx-5", 2020, "blue", 22000)
-# Car.print_cars_id()
-# # добавляем рег. номер через сеттер
-# car2.setRegNumber("33-mm-55")
-# print(f" {car2.brand}, {car2.model}, {car2.year}, {car2.year}, {car2.price}, {car2.getRegNumber()}")
-# print(Car.customer_discount(car2.price))
+# добавляем рег. номер через сеттер
+car1.setRegNumber("22-ff-44")
+print(f" {car1.brand}, {car1.model}, {car1.year}, {car1.year}, {car1.price}, {car1.getRegNumber()}")
+# посчитаем скидку
+print(f" price with discount {Car.customer_discount(car1.price)}")
 
+# car2 = Car("mazda", "cx-5", 2020, "blue", 22000)
+# Car.print_cars_id()
+
+# добавляем рег. номер через сеттер
+car2.setRegNumber("33-mm-55")
+print(f" {car2.brand}, {car2.model}, {car2.year}, {car2.year}, {car2.price}, {car2.getRegNumber()}")
+print(f" price with discount {Car.customer_discount(car2.price)}")
